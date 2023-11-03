c8e26945f77d0bda3875883a704e69b953378e5d
M_Mek <mmekraz781@gmail.com>
2023-11-03 14:44:36 -0400

    added potluckitems assign function

--- js/script.js
+++ js/script.js
@@ -10,6 +10,10 @@ const guestList = document.querySelector(".guest-list");
 const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
+// assign button (not yet visible)
+const assignButton = document.querySelector(".assign");
+// assigned items list (not yet visible)
+const assignedItemsList = document.querySelector(".assigned-items");
 
 const clearInput = function () {
   guestInput.value = "";
@@ -21,15 +25,6 @@ const addToList = function (guest) {
   guestList.append(listItem);
 };
 
-addGuestButton.addEventListener("click", function () {
-  const guest = guestInput.value;
-  if (guest !== "") {
-    addToList(guest);
-    clearInput();
-    updateGuestCount();
-  }
-});
-
 const updateGuestCount = function () {
   const guests = document.querySelectorAll(".guest-list li");
   guestCount.innerText = guests.length;
@@ -40,3 +35,39 @@ const updateGuestCount = function () {
     guestFull.classList.remove("hide");
   }
 };
+
+const assignItems = function () {
+  const potluckItems = [
+    "Veggie Nori Rolls",
+    "Tomato & Mozzarella Caprese Skewers",
+    "3-Cheese Tomato Tart",
+    "Pesto Pasta Salad",
+    "Tangy Carrot Slaw",
+    "Sweet and Salty Watermelon Fruit Salad",
+    "The Ultimate Grilled Zucchini Salad",
+    "Lemon-Garlic Whipped Feta with Pesto",
+  ];
+  const allGuests = document.querySelectorAll(".guest-list li");
+  for (let guest of allGuests) {
+    const randomPotluckIndex = Math.floor(Math.random() * potluckItems.length);
+    const randomPotluckItem = potluckItems[randomPotluckIndex];
+    const listItem = document.createElement("li");
+    listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}.`;
+    assignedItemsList.append(listItem);
+    potluckItems.splice(randomPotluckIndex, 1);
+  }
+};
+
+addGuestButton.addEventListener("click", function () {
+  const guest = guestInput.value;
+  if (guest !== "") {
+    addToList(guest);
+    clearInput();
+    updateGuestCount();
+  }
+});
+
+assignButton.addEventListener("click", function () {
+  assignItems();
+  assignButton.disabled = true;
+});
